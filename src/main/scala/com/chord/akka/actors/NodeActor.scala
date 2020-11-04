d0667782ca1045ec734dbf6ade6f6fc83c6a5721
Prajwal K Kammardi <pkamma2@uic.edu>
2020-11-04 11:57:51 -0600

    Made small changes to actors and changed logging in logback.xml

--- src/main/scala/com/chord/akka/actors/NodeActor.scala
+++ src/main/scala/com/chord/akka/actors/NodeActor.scala
@@ -3,29 +3,33 @@ package com.chord.akka.actors
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import com.chord.akka.actors.NodeActor.Command
-import com.typesafe.scalalogging.LazyLogging
 
 
-object NodeActor  {
+object NodeActor {
 
-  def apply(nodeId:String): Behavior[Command] =
-    Behaviors.setup(context => new NodeActor(context,nodeId))
+  def apply(nodeId: String): Behavior[Command] =
+    Behaviors.setup(context => new NodeActor(context, nodeId))
 
   sealed trait Command
+
   final case class addNodesToChordRing(num_users: Int) extends Command
+
 }
+
 // Need to implement successor , fingerTable
-class NodeActor(context: ActorContext[Command],chordNodeId:String) extends AbstractBehavior[Command](context) with LazyLogging {
+class NodeActor(context: ActorContext[Command], chordNodeId: String) extends AbstractBehavior[Command](context) {
+
   import NodeActor._
+
   override def onMessage(msg: Command): Behavior[Command] =
     msg match {
       case addNodesToChordRing(n) =>
-        logger.info("Creating "+n+" Users")
+        context.log.info(s"Creating $n Nodes")
         val nodeList = new Array[String](n)
-        for(i<- 0 until n){
-          val nodeId: String = "Node-"+i
+        for (i <- 0 until n) {
+          val nodeId: String = "Node-" + i
           nodeList(i) = nodeId
-          context.spawn(NodeActor(nodeId),nodeId)
+          context.spawn(NodeActor(nodeId), nodeId)
         }
         this
 
--- src/main/scala/com/chord/akka/actors/UserActor.scala
+++ src/main/scala/com/chord/akka/actors/UserActor.scala
@@ -4,28 +4,31 @@ package com.chord.akka.actors
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import com.chord.akka.actors.UserActor.Command
-import com.typesafe.scalalogging.LazyLogging
 
-object UserActor  {
+object UserActor {
 
-  def apply(id:String): Behavior[Command] =
-    Behaviors.setup(context => new UserActor(context,id))
+  def apply(id: String): Behavior[Command] =
+    Behaviors.setup(context => new UserActor(context, id))
 
   sealed trait Command
+
   final case class createUsers(num_users: Int) extends Command
+
 }
 
-class UserActor(context: ActorContext[Command],id:String) extends AbstractBehavior[Command](context) with LazyLogging {
+class UserActor(context: ActorContext[Command], id: String) extends AbstractBehavior[Command](context) {
+
   import UserActor._
+
   override def onMessage(msg: Command): Behavior[Command] =
     msg match {
       case createUsers(n) =>
-        logger.info("Creating "+n+" Users")
+        context.log.info(s"Creating $n Users")
         val userList = new Array[String](n)
-        for(i<- 0 until n){
-          val userId: String = "User-"+i
+        for (i <- 0 until n) {
+          val userId: String = "User-" + i
           userList(i) = userId
-          context.spawn(UserActor(userId),userId)
+          context.spawn(UserActor(userId), userId)
         }
         this
     }
