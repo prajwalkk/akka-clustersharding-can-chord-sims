3d64e2d568716589c3d7b4aa5fc9faf62d374a7a
Rishabh <rmehta35@uic.edu>
2020-11-03 18:15:49 -0600

    Skeletal code for creating users and nodes in Simulation through Behaviours defined in Simulation

--- src/main/scala/com/chord/akka/SimulationDriver.scala
+++ src/main/scala/com/chord/akka/SimulationDriver.scala
@@ -1,12 +1,13 @@
 package com.chord.akka
+
 import com.chord.akka.simulation.Simulation
+
 object SimulationDriver {
 
 
   def main(args: Array[String]): Unit = {
     val simulation = Simulation
   }
-
 
 
 }
--- src/main/scala/com/chord/akka/simulation/Simulation.scala
+++ src/main/scala/com/chord/akka/simulation/Simulation.scala
@@ -8,25 +8,38 @@ import com.typesafe.scalalogging.LazyLogging
 
 object Simulation extends LazyLogging {
 
-  final case class addNodesToChordRing(num_nodes:Int)
+  sealed trait Command
+
+  final case class addNodesToChordRing(num_nodes: Int) extends Command
+
+  final case class createUsers(num_users: Int) extends Command
 
 
   val config: Config = ConfigFactory.load("Simulator.conf")
   val num_nodes: Int = config.getInt("NUM_NODES")
+  val num_users: Int = config.getInt("NUM_USERS")
   logger.info("Creating Chord Actor System")
-  val chordActorSystem :ActorSystem[Simulation.addNodesToChordRing]=  ActorSystem(Simulation(),"ChordActorSystem")
-
+  val chordActorSystem: ActorSystem[Simulation.addNodesToChordRing] = ActorSystem(Simulation(), "ChordActorSystem")
   chordActorSystem ! addNodesToChordRing(num_nodes)
+  logger.info("Creating User Actor System")
+  val userActorSystem: ActorSystem[Simulation.createUsers] = ActorSystem(Simulation(), "UserActorSystem")
+  userActorSystem ! createUsers(num_users)
 
-  def apply(): Behavior[addNodesToChordRing] =
-    Behaviors.setup { _ =>
-      Behaviors.receiveMessage { message =>
-        logger.info(message.num_nodes.toString)
-        //create ChordNodes
-        Behaviors.same
+
+  def apply(): Behavior[Command] =
+    Behaviors.setup { context =>
+      logger.info(context.toString)
+      Behaviors.receiveMessage {
+        case addNodesToChordRing(n: Int) =>
+          logger.info(n.toString)
+          //create ChordNodes
+          Behaviors.same
+        case createUsers(n: Int) =>
+          logger.info(n.toString)
+          //create UserNodes
+          Behaviors.same
       }
     }
-
 
 
 }
