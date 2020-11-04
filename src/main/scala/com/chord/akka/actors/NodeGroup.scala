1cc86df28f42f0945844c6c142f7f50966f8c35a
Prajwal K Kammardi <pkamma2@uic.edu>
2020-11-04 17:33:31 -0600

    Added UserGroups and NodeGroups to handle multiple actors

---
+++ src/main/scala/com/chord/akka/actors/NodeGroup.scala
@@ -0,0 +1,13 @@
+package com.chord.akka.actors
+
+/*
+*
+* Created by: prajw
+* Date: 04-Nov-20
+*
+*/
+object NodeGroup {
+
+}
+
+
---
+++ src/main/scala/com/chord/akka/webserver/JsonFormats.scala
@@ -0,0 +1,15 @@
+package com.chord.akka.webserver
+
+
+
+//#json-formats
+
+object JsonFormats  {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+
+  //implicit val userJsonFormat = jsonFormat3(User)
+  //implicit val usersJsonFormat = jsonFormat1(Users)
+
+  //implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
+//#json-formats
