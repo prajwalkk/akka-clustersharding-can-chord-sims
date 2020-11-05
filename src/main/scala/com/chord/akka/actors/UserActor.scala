1378111d5e78d14359d40ab21795dfd247bf82fe
Rishabh <rmehta35@uic.edu>
2020-11-04 18:02:53 -0600

    Combining UserGroup and UserActor to eliminate extra reply

--- src/main/scala/com/chord/akka/actors/UserActor.scala
+++ src/main/scala/com/chord/akka/actors/UserActor.scala
@@ -2,22 +2,23 @@ package com.chord.akka.actors
 
 
 
-import UserGroup.UsersCreated
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import com.chord.akka.actors.UserActor.Command
+import com.chord.akka.utils.SystemConstants
 
 
 
 object UserActor {
 
-
+  var userList = new Array[String](SystemConstants.num_users)
   def apply(id: String): Behavior[Command] =
     Behaviors.setup(context => new UserActor(context, id))
 
   sealed trait Command
 
-  final case class createUsers(num_users: Int,replyTo:ActorRef[UsersCreated]) extends Command
+  final case class createUsers(num_users: Int) extends Command
+
 
 
 }
@@ -28,17 +29,18 @@ class UserActor(context: ActorContext[Command], id: String) extends AbstractBeha
 
   override def onMessage(msg: Command): Behavior[Command] =
     msg match {
-      case createUsers(n,replyTo) =>
+      case createUsers(n) =>
         context.log.info(s"Creating $n Users")
-        val userList = new Array[String](n)
+
         for (i <- 0 until n) {
           val userId: String = "User-" + i
           val user = context.spawn(UserActor(userId), userId)
           userList(i) = user.path.toString
           context.log.info("User Created "+userList(i))
         }
-        replyTo ! UsersCreated(userList)
+
         this
+
 
     }
 }
--- src/main/scala/com/chord/akka/simulation/Simulation.scala
+++ src/main/scala/com/chord/akka/simulation/Simulation.scala
@@ -1,16 +1,11 @@
 package com.chord.akka.simulation
 
-import com.chord.akka.utils.SystemConstants
 import akka.actor.typed.ActorSystem
-import com.chord.akka.actors
 import com.chord.akka.actors.NodeActor.createNodes
 import com.chord.akka.actors.UserActor.createUsers
-import com.chord.akka.actors.UserGroup.UsersCreated
-import com.chord.akka.actors.{NodeActor, UserActor, UserGroup}
-import com.typesafe.config.{Config, ConfigFactory}
+import com.chord.akka.actors.{NodeActor, UserActor}
+import com.chord.akka.utils.SystemConstants
 import com.typesafe.scalalogging.LazyLogging
-
-import scala.concurrent.{Await, ExecutionContext, Future}
 
 object Simulation extends LazyLogging {
 
@@ -20,10 +15,9 @@ object Simulation extends LazyLogging {
   chordActorSystem ! createNodes(SystemConstants.num_nodes)
   logger.info("Creating User Actor System")
   val userActorSystem: ActorSystem[UserActor.Command] = ActorSystem(UserActor("UserActorSystem"), "UserActorSystem")
-  val userGroup: ActorSystem[UserGroup.Command] = ActorSystem(UserGroup(), "UserGroup")
-  userActorSystem ! createUsers(SystemConstants.num_users,userGroup.ref)
+  userActorSystem ! createUsers(SystemConstants.num_users)
   Thread.sleep(1000)
-  logger.info(UserGroup.userList.toSeq.toString())
+  logger.info(UserActor.userList.toSeq.toString())
 
 
 
