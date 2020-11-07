6d180c0b270a1ae4a16bf175cb76b98e98381219
Rishabh <rmehta35@uic.edu>
2020-11-06 19:30:03 -0600

    Added UserRoutes.scala [partial] need to uncomment userRegistory in HttpServer for userhttp to work

--- src/main/scala/com/chord/akka/webserver/JsonFormats.scala
+++ src/main/scala/com/chord/akka/webserver/JsonFormats.scala
@@ -1,5 +1,5 @@
 package com.chord.akka.webserver
-import com.chord.akka.actors.{LookupObject, LookupObjects, NodeGroup}
+import com.chord.akka.actors.{LookupObject, LookupObjects, NodeGroup, lookup_reply}
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 
@@ -14,5 +14,7 @@ object JsonFormats  {
   implicit val usersJsonFormat: RootJsonFormat[LookupObjects] = jsonFormat1(LookupObjects)
 
   implicit val actionPerformedJsonFormat: RootJsonFormat[NodeGroup.ActionSuccessful] = jsonFormat1(NodeGroup.ActionSuccessful)
+  implicit  val lookup_replyJson = jsonFormat1(lookup_reply)
+
 }
 //#json-formats
